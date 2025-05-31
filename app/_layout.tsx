7e9a541c68c1633caab7df68b8333d915cff66d4
Paz <pasharakha.255@gmail.com>
2025-06-01 00:29:32 +0700

    Sampe sini notif aman

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,13 +1,25 @@
+import { NotificationProvider } from "@/context/NotificationContext";
 import { useFonts } from "expo-font";
+import * as Notifications from "expo-notifications";
 import { SplashScreen, Stack } from "expo-router";
 import { useEffect } from "react";
 
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldPlaySound: true,
+    shouldSetBadge: false,
+    shouldShowAlert: true,
+    shouldShowBanner: true,
+    shouldShowList: true,
+  }),
+});
+
 export default function RootLayout() {
   const [fontsLoaded] = useFonts({
-    'Montage': require('../assets/fonts/Montage-Demo.ttf'),
-    'Poppins-Regular': require('../assets/fonts/Poppins-Regular.ttf'),
-    'Poppins-Medium': require('../assets/fonts/Poppins-Medium.ttf'),
-    'Poppins-Semibold': require('../assets/fonts/Poppins-SemiBold.ttf'),
+    Montage: require("../assets/fonts/Montage-Demo.ttf"),
+    "Poppins-Regular": require("../assets/fonts/Poppins-Regular.ttf"),
+    "Poppins-Medium": require("../assets/fonts/Poppins-Medium.ttf"),
+    "Poppins-Semibold": require("../assets/fonts/Poppins-SemiBold.ttf"),
   });
 
   useEffect(() => {
@@ -23,10 +35,12 @@ export default function RootLayout() {
   }
 
   return (
-    <Stack
-      screenOptions={{
-        headerShown: false,
-      }}
-    />
+    <NotificationProvider>
+      <Stack
+        screenOptions={{
+          headerShown: false,
+        }}
+      />
+    </NotificationProvider>
   );
 }
